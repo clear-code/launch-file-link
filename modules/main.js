e6c1a800aa2581b750f233cc59487a3ead57aeb4
YUKI Hiroshi <shimoda@clear-code.com>
2014-04-25 18:55:17 +0900

    Show confirmation dialog before open the file

--- modules/main.js
+++ modules/main.js
@@ -4,6 +4,13 @@
 
 load('lib/WindowManager');
 
+Components.utils.import('resource://gre/modules/Services.jsm');
+
+var bundle = require('lib/locale')
+               .get('chrome://launch-file-link/locale/launch-file-link.properties');
+
+var lastState = new WeakMap();
+
 function handleLinkClick(aEvent) {
   var target = aEvent.originalTarget;
   while (target && !target.href) {
@@ -16,29 +23,64 @@ function handleLinkClick(aEvent) {
   if (url.indexOf('file:') != 0)
     return true;
 
-  var IOService = Cc['@mozilla.org/network/io-service;1']
-                    .getService(Ci.nsIIOService);
-  var FileHandler = IOService.getProtocolHandler('file')
+  aEvent.stopPropagation();
+  aEvent.preventDefault();
+
+  var FileHandler = Services.io.getProtocolHandler('file')
                       .QueryInterface(Ci.nsIFileProtocolHandler);
   try {
     var file = FileHandler.getFileFromURLSpec(url);
-    file.QueryInterface(Ci.nsILocalFile).launch();
-    aEvent.stopPropagation();
-    aEvent.preventDefault();
-    return false;
+    file = file.QueryInterface(Ci.nsILocalFile);
+    var view = target.ownerDocument.defaultView;
+
+    var confirmed = lastState.get(view);
+    if (confirmed == undefined) {
+      let messageType = file.isDirectory() ? 'folder' : 'file';
+      let checked = { value: false };
+      confirmed = Services.prompt.confirmCheck(
+        view,
+        bundle.getString('confirm.title'),
+        bundle.getFormattedString('confirm.message.' + messageType, [file.parent.path, file.leafName]),
+        bundle.getString('confirm.check'),
+        checked
+      );
+      if (checked.value)
+        lastState.set(view, confirmed);
+    }
+
+    if (!confirmed)
+      return false;
+
+    file.launch();
   }
   catch(aError) {
     Cu.reportError(new Error('failed to open a file URL <' + url + '>\n' + aError.message));
-    return true;
+  }
+  finally {
+    return false;
   }
 }
 
-function handleWindow(aWindow)
-{
+function handleSubWindowUnload(aEvent) {
+  var view = aEvent.originalTarget;
+  view = view.ownerDocument || view;
+  view = view.defaultView;
+  lastState.delete(view);
+}
+
+function handleWindowUnload(aEvent) {
+  var view = aEvent.originalTarget;
+  view = view.ownerDocument || view;
+  view = view.defaultView;
+  lastState.delete(view);
+  view.removeEventListener('unload', handleSubWindowUnload, true);
+  view.removeEventListener('click', handleLinkClick, true);
+}
+
+function handleWindow(aWindow) {
   aWindow.addEventListener('click', handleLinkClick, true);
-  aWindow.addEventListener('unload', function() {
-    aWindow.removeEventListener('click', handleLinkClick, true);
-  }, false);
+  aWindow.addEventListener('unload', handleSubWindowUnload, true);
+  aWindow.addEventListener('unload', handleWindowUnload, false);
 }
 
 WindowManager.getWindows().forEach(handleWindow);
@@ -48,11 +90,16 @@ function shutdown()
 {
   WindowManager.getWindows().forEach(function(aWindow) {
     aWindow.removeEventListener('click', handleLinkClick, true);
+    aWindow.removeEventListener('unload', handleSubWindowUnload, true);
+    aWindow.removeEventListener('unload', handleWindowUnload, false);
   });
   WindowManager.removeHandler(handleWindow);
 
   handleWindow = undefined;
   handleLinkClick = undefined;
+  bundle = undefined;
+  lastState = undefined;
 
+  Services = undefined;
   WindowManager = undefined;
 }
